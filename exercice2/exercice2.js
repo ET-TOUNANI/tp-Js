07e9b51d0a16e96fc179b6eda6b3597fc3f5b254
ET-TOUNANI <tounaniabderrahmane99@gmail.com>
2022-03-25 12:19:39 +0100

    finish ex2 100%

--- exercice2/exercice2.js
+++ exercice2/exercice2.js
@@ -1,40 +1,51 @@
 
-let values=document.querySelectorAll(".val")
-let datas=[];
-const ctx = document.getElementById('histogram').getContext('2d');
-for(i=0;i<values.length;i++){
-    datas.push(parseInt(values[i].value));
-}
-const chart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["Monday", "tuesday", "thursday", "wednsday", "friday","saturday","sunday"],
-    datasets: [{
-      label: 'Number of Arrivals',
-      data: datas,
-      backgroundColor: 'green',
-    }]
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        display: false,
-        barPercentage: 0.5,
-        ticks: {
-          max: 3,
-        }
-      }, {
-        display: true,
-        ticks: {
-          autoSkip: false,
-          max: 4,
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    }
+
+
+const cnv=document.getElementById('histogram');
+const ctx = cnv.getContext('2d');
+
+
+
+function generer(){
+  let values=document.querySelectorAll(".val")
+  let datas=[];
+  for(i=0;i<values.length;i++){
+      datas.push(parseInt(values[i].value));
   }
-});
+  container=document.querySelector(".container");
+  cnv.style.display="block";
+  container.innerHTML="<button class='btn btn-primary' onclick='window.location.reload(true)'>back</button>";
+  const chart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ["Monday", "tuesday", "thursday", "wednsday", "friday","saturday","sunday"],
+        datasets: [{
+          label: 'Number of Arrivals',
+          data: datas,
+          backgroundColor: 'green',
+        }]
+      },
+      options: {
+        scales: {
+          xAxes: [{
+            display: false,
+            barPercentage: 0.5,
+            ticks: {
+              max: 3,
+            }
+          }, {
+            display: true,
+            ticks: {
+              autoSkip: false,
+              max: 4,
+            }
+          }],
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+}
